ac6f59fb7ca17ee9cdabf443c016378d86af9acf
ZeljkoSkipic <zeljko.skipic@gmail.com>
2022-11-29 13:40:49 +0100

    Update footer.php

--- footer.php
+++ footer.php
@@ -22,7 +22,7 @@
 			<span class="sep"> | </span>
 				<?php
 				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'ilogic' ), 'ilogic', '<a href="http://underscores.me/">Underscores.me</a>' );
+				printf( esc_html__( 'Theme: %1$s by %2$s.', 'ilogic' ), 'ilogic', '<a href="https://lemon-mss.com/">Lemon MSS</a> & <a href="https://zeljkoskipic.dev">Zeljko Skipic</a>' );
 				?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
